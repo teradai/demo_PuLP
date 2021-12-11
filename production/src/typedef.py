8ffc40028044d6b11e1042144d2a3b7943366975
teradai <dai.1114.soccer.13@gmail.com>
2021-12-11 16:17:15 +0900

    typing.TypedDictを使った型定義

--- production/src/typedef.py
+++ production/src/typedef.py
@@ -4,4 +4,13 @@ class Item(typing.TypedDict):
     name: str
     job: list[int]
     profit: int
-    
+
+
+class ResultItem(typing.TypedDict):
+    name: str
+    value: int
+
+class Result(typing.TypedDict):
+    status: str
+    objective_value: float
+    items: list[ResultItem]
